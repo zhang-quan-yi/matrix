a58aa01f3d5f094b478cdf0f7f261ea81139ed5b
zhangquanyi <zh.hailei@gmail.com>
2016-04-04 21:13:27 +0800

    非正式版v0.0

--- gruntfile.js
+++ gruntfile.js
@@ -1,16 +1,63 @@
 module.exports = function(grunt){
-	grunt.initConfig({
+	var cwd = process.cwd();
+	var config = {
 		pkg: grunt.file.readJSON("package.json"),
-		uglify: {
+		jshint: {
 			options: {
-				banner: '/<%= pkg.name%> <%= grunt.template.today("yyyy-mm-dd") %>/\n'
+				ignores: ['common/js/*.min.js']
 			},
-			build: {
-				src: 'src/<%= pkg.name %>.js',
-				dest: 'build/<%= pkg.name %>.min.js'
+			check: {
+				files: {
+					src: ['source/js/*.js','common/js/*.js']
+				}
 			}
+		},
+		//CSS相关
+		sass:{
+				options:{
+					//compressed|nested|compact|expanded
+					style: 'compressed',
+					//if only compile changed files
+					update: false,
+					//no cache to the sass files
+					noCache: true
+				},
+				files:{
+					//expand Set to true will enable the 
+					//following properties
+					expand: true,
+					cwd: 'source/sass/final-sass/',
+					src: ['*.sass'],
+					//this dir is base gruntfile.js
+					//not the cwd dir...
+					//what a fuck
+					dest: 'web/css/',
+					ext: '.css'
+				}
+			
+		},
+		requirejs:{
+			debug: grunt.file.readJSON('require.json'),
+			release: grunt.file.readJSON('require.json')
 		}
-	});
+	};
+	config.requirejs.debug.options.uglify2 = {
+		"output": {
+			"beautify": true,
+			"comments": true,
+			"ascii_only": false		
+		},
+		"compress": false,
+		"mangle": false
+
+	};
+	grunt.initConfig(config);
+	//process.chdir('../');
+	
 	grunt.loadNpmTasks('grunt-contrib-uglify');
-	grunt.registerTask('default',['uglify']);
+	grunt.loadNpmTasks('grunt-contrib-sass');
+	grunt.loadNpmTasks('grunt-contrib-requirejs');
+	grunt.loadNpmTasks('grunt-contrib-jshint');
+	
+	grunt.registerTask('default',['sass']);
 }; 
